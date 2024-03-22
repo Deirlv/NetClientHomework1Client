fe1105a11e19e9879169454d6cdb7b1e66b81fc2
Zuba_ji34@student.itstep.org <Zuba_ji34@student.itstep.org>
2024-03-22 15:08:24 +0100

    Add project files.

---
+++ NetClientHomework1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Reflection;
+using System.Globalization;
+
+namespace NetClientHomework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IPAddress address = IPAddress.Parse("192.168.178.34");
+
+            IPEndPoint endPoint = new IPEndPoint(address, 8080);
+
+            while(true)
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+                try
+                {
+                    socket.Connect(endPoint);
+                    if (socket.Connected)
+                    {
+                        Console.Write("Enter message: ");
+                        string message = Console.ReadLine();
+
+                        socket.Send(Encoding.Default.GetBytes(message)); //
+
+                        string query = "GET\r\n\r\n";
+                        socket.Send(Encoding.Default.GetBytes(query));
+
+                        byte[] buffer = new byte[1024];
+
+                        int len;
+
+                        do
+                        {
+                            len = socket.Receive(buffer);
+                            Console.WriteLine(Encoding.Default.GetString(buffer, 0, len));
+                        } while (socket.Available > 0);
+                    }
+                    else
+                    {
+                        Console.Error.WriteLine("Error");
+                    }
+                }
+                finally
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+            }
+            
+        }
+    }
+}
---
+++ ServerProject/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace ServerProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("192.168.178.34");
+
+            IPEndPoint endPoint = new IPEndPoint(ipAddress, 1024);
+
+            Socket pass_socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            pass_socket.Bind(endPoint);
+            pass_socket.Listen(10);
+
+            Console.WriteLine($"Server started at port {endPoint.Port}");
+
+            try
+            {
+                while (true)
+                {
+                    Socket ns = pass_socket.Accept();
+
+                    Console.WriteLine($"Client {ns.LocalEndPoint} was connected");
+                    Console.WriteLine($"Client {ns.RemoteEndPoint} was connected");
+
+                    ns.Send(Encoding.Default.GetBytes($"Server {ns.LocalEndPoint} sent answer in {DateTime.Now}\n"));
+
+                    ns.Shutdown(SocketShutdown.Both);
+                    ns.Close();
+                }
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
